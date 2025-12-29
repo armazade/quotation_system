ae8e8f76c1d4f6f9761843a8a3915b9ed3d87241
Arman Khodadadzade <armantje2001@gmail.com>
2025-12-29 14:58:35 +0100

    update dependencies and IDE helpers
    Updated composer.json, package-lock.json, and composer.lock to reflect dependency changes. Regenerated _ide_helper.php and added _ide_helper_models.php for improved IDE support. Updated several Vue authentication pages, likely for compatibility or improvements.

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,9 +1,9 @@
 <script setup>
 import GuestLayout from '@/Layouts/GuestLayout.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
+import InputError from '@/Components/Form/Elements/InputError.vue';
+import InputLabel from '@/Components/Form/Elements/InputLabel.vue';
+import PrimaryButton from '@/Components/Buttons/PrimaryButton.vue';
+import TextInput from '@/Components/Form/Elements/TextInput.vue';
 import { Head, Link, useForm } from '@inertiajs/vue3';
 
 const form = useForm({
